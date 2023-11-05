6eb0d8de90cb186f5544ef03a49e3a72c7120d5c
imLaoYang <623628984@qq.com>
2023-11-05 11:27:08 +0800

    基本模块构建

---
+++ im-common/src/main/java/com/yang/im/common/A.java
@@ -0,0 +1,4 @@
+package com.yang.im.common;
+
+public class A {
+}
---
+++ im-service/src/main/java/com/yang/im/service/config/BaseConfig.java
@@ -0,0 +1,8 @@
+package com.yang.im.service.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class BaseConfig {
+}
